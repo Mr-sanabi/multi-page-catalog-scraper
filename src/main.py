20f4e4c61a0e3faa26573faa63ea449b86be636c
Mr-sanabi <v2839630@gmail.com>
2026-07-09 11:27:38 +0200

    Parse books from one catalog page

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fetcher import fetch_html
+from parser import parse_books
 
 def main():
     test_url = "https://books.toscrape.com/catalogue/page-1.html"
     html = fetch_html(test_url)
-    print(html[:500])
+    books = parse_books(html)
+    print(len(books))
+    print(books[:3])
 
 
 if __name__ == "__main__":
---
+++ src/parser.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+
+def get_text_or_empty(parent, selector):
+    tag = parent.select_one(selector)
+    if tag:
+        return tag.get_text(strip=True)
+    else:
+        return ""
+    
+def get_attr_or_empty(parent, selector, attr):
+    tag = parent.select_one(selector)
+    if tag and tag.has_attr(attr):
+        return tag[attr]
+    else:
+        return ""
+    
+def extract_book(card):
+    title = get_attr_or_empty(card, "h3 a", "title")
+    price = get_text_or_empty(card, ".price_color")
+    availability = get_text_or_empty(card, ".availability")
+    rating_tag = card.select_one("p.star-rating")
+    product_url = get_attr_or_empty(card, "h3 a", "href")
+
+    if rating_tag and rating_tag.has_attr("class"):
+        rating = rating_tag["class"][1]
+    else:
+        return ""
+    
+    return {
+        "title": title,
+        "price": price,
+        "availability": availability,
+        "rating": rating,
+        "prodcut_url": product_url
+    }
+    
+
+def parse_books(html):
+    soup = BeautifulSoup(html, "html.parser")
+    cards = soup.select(".product_pod")
+    books = []
+    for card in cards:
+        books_list = extract_book(card)
+        books.append(books_list)
+
+    return books
