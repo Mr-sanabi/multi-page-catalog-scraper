6cc46bcf61d7f5c7ade0f5b0752312bfb4d5d6b8
Mr-sanabi <v2839630@gmail.com>
2026-07-09 11:33:28 +0200

    Add multi-page scraping flow

--- src/main.py
+++ src/main.py
@@ -3,10 +3,22 @@ from parser import parse_books
 
 def main():
     test_url = "https://books.toscrape.com/catalogue/page-1.html"
-    html = fetch_html(test_url)
-    books = parse_books(html)
-    print(len(books))
-    print(books[:3])
+
+    
+    all_books = []
+    max_pages = 3
+
+    for page_number in range(1, max_pages + 1):
+        page_url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
+        html = fetch_html(page_url)
+        print(page_url)
+        books = parse_books(html)
+
+        all_books.extend(books)
+
+    print(len(all_books))
+    
+    
 
 
 if __name__ == "__main__":
